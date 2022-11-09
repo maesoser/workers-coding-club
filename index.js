807115047287ece2873a8dca2b5c2c85b66ca412
Sergio Maeso <sergio@cloudflare.com>
2022-11-09 01:06:18 +0000

    first_assignment: return a different answer when a POST request arrives, using json format if possible

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 export default {
 	fetch(request) {
-		console.log("[" + request.method + "] " +request.url)
-		if (request.url === "https://ny-firsker.massesos.workers.dev/"){
-		return new Response('Hello xxx!, the url is ' + request.url, {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
-	}else{
-		console.log("else was triggered")
-		return new Response('Wrong ... URL:' + request.url)
+		console.log("[" + request.method + "] " + request.url)
+		let response = {
+			"method": request.method,
+			"url": request.url,
+		}
+		if (request.method === "POST"){
+			response["success"] = true
+		}else{
+			response["success"] = false
+		}
+		return new Response(
+			JSON.stringify(response, null, 4), 
+			{ headers: { 'content-type': 'application/json', }, }
+		);
 	}
-	},
 };
